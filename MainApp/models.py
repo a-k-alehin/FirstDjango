b3492c4a0c650d4d0442adb3c8d0fe5037a8f94b
a.k.alehin <a.k.alehin@gmail.com>
2023-12-16 14:10:38 +0300

    many-to-many

--- MainApp/models.py
+++ MainApp/models.py
@@ -2,12 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+class Color(models.Model):
+    name  = models.CharField(max_length=32)
+    def __repr__(self) -> str:
+        return f'Color({self.name})'
+
 class Item(models.Model):
     name  = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     count = models.PositiveIntegerField()
     descr = models.CharField(max_length=500, blank=True, default="нет описания")
+    colors = models.ManyToManyField(to=Color)
 
     def __repr__(self):
-        return f'{self.id} {self.name} {self.brand} {self.count}'
+        colors = [c.name for c in self.colors.all()]
+        return f'{self.id} {self.name} {self.brand} {self.count} {colors}'
     
--- README.md
+++ README.md
@@ -11,4 +11,19 @@ https://stackoverflow.com/questions/54639428/v-s-code-v1-31-emmet-intellisense-n
 
 
 
-python manage.py dumpdata --indent 4 MainApp >data/MainApp.json
+    python manage.py dumpdata --indent 4 MainApp >data/MainApp.json
+
+
+
+
+    python manage.py shell_plus --bpython
+    c_red = Color(1,'red')
+    c_red.save()
+    i = Item.objects.get(id=1)
+    i.colors.add(c_red)
+    i.save()
+    i.colors.all() #показать цвета
+    # отфильтровать
+    i = Item.objects.filter(colors__name='green')
+    
+
